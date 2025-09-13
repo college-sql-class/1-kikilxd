b2367cf7f382012692511c1e14777ed04bf06448
kikilxd <kikilxdx@proton.me>
2025-09-13 15:17:47 +0300

    add SQL requests

--- starter.sql
+++ starter.sql
@@ -3,3 +3,8 @@
 
 -- UPDATE где SET что WHERE условие;
 -- DELETE FROM где WHERE условие;
+
+UPDATE students SET name = 'Alice_updated' WHERE id = 1;
+UPDATE students SET age = 22 WHERE id = 1;
+
+DELETE FROM students WHERE id = 2;
